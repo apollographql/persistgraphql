d81320eca7479eaf01e4d7f3736f63a7a9005927
Dhaivat Pandya <dhaivatpandya@gmail.com>
2016-10-14 23:43:08 -0400

    added more utility methods and general structure of ExtractGQL

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import fs = require('fs');
+import path = require('path');
 
-export interface OutputMap {
-
-}
+// TODO figure out how graphql-tag/parser is supposed to be imported
+// and use that instead of the graphql package.
+import { 
+  parse,
+} from 'graphql';
 
 export class ExtractGQL {
   public inputFilePath: string;
@@ -10,8 +13,26 @@ export class ExtractGQL {
 
   // Given a file path, this returns the extension of the file within the
   // file path.
-  public static getFileExtension() {
-    // pass
+  public static getFileExtension(filePath: string): string {
+    const pieces = path.basename(filePath).split('.');
+    if (pieces.length <= 1) {
+      return "";
+    }
+    return pieces[pieces.length - 1];
+  }
+
+  // Reads a file into a string.
+  public static readFile(filePath: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if(err) {
+          console.log('Error: ', err);
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
   }
 
   // Checks if a given path points to a directory.
@@ -37,16 +58,44 @@ export class ExtractGQL {
     this.inputFilePath = inputFilePath;
     this.outputFilePath = outputFilePath;
   }
+
+  public processGraphQLFile(graphQLFile: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+
+    });
+  }
+
+  public processInputFile(inputFile: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const extension = ExtractGQL.getFileExtension(inputFile);
+      switch(extension) {
+        case 'graphql':
+        this.processGraphQLFile(inputFile);
+        break;
+
+        default:
+        resolve(false);
+        break;
+      }
+    });
+  } 
+
   // Processes an input path, which may be a path to a GraphQL file,
   // a TypeScript file or a Javascript file. Returns a map going from
   // a hash to a query document.
-  public processInputPath() {
-    // pass 
+  public processInputPath(inputPath: string): Promise<boolean> {
+    return new Promise<void>((resolve, reject) => {
+      if (ExtractGQL.isDirectory(inputPath)) {
+        // TODO recurse over the files in this directory
+      } else {
+        this.processInputFile(inputPath);
+      }
+    });
   }
 
   // Extracts GraphQL queries from this.inputFilePath and produces
   // an output JSON file in this.outputFilePath.
   public extract() {
-    this.processInputPath();
+    this.processInputPath(this.inputFilePath);
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,8 @@ import * as chai from 'chai';
 const { assert } = chai;
 
 import { ExtractGQL } from '../src/index';
+import { parse, print } from 'graphql';
+import gql from 'graphql-tag';
 
 describe('ExtractGQL', () => {
   it('should be able to construct an instance', () => {
@@ -15,7 +17,7 @@ describe('ExtractGQL', () => {
 
   describe('isDirectory', () => {
     it('should return true on a directory', (done) => {
-      ExtractGQL.isDirectory('./test/fixtures').then((result) => {
+      ExtractGQL.isDirectory('./test/fixtures').then((result: boolean) => {
         assert(result);
         done();
       });
@@ -24,6 +26,36 @@ describe('ExtractGQL', () => {
     it('should return false on a file', (done) => {
       ExtractGQL.isDirectory('./test/fixtures/queries.graphql').then((result) => {
         assert(!result);
+        done();
+      });
+    });
+  });
+
+  describe('getFileExtension', () => {
+    it('should return the correct extension on a file with an extension', () => {
+      assert.equal(ExtractGQL.getFileExtension('../../path/source.graphql'), 'graphql');
+      assert.equal(ExtractGQL.getFileExtension('/some/complicated/path.with.dots/dots../view.js'), 'js');
+    });
+    
+    it('should return an empty string if the file has no extension', () => {
+      assert.equal(ExtractGQL.getFileExtension('/redherring.graphql/file'), '');
+      assert.equal(ExtractGQL.getFileExtension('file'), '');
+    });
+  });
+
+  describe('readFile', () => {
+    it('should be able to read a file into a string', (done) => {
+      const filePath = 'test/fixtures/queries.graphql';
+      ExtractGQL.readFile(filePath).then((result) => {
+        const graphQLString = print(parse(result));
+        assert.deepEqual(graphQLString, print(gql`
+          query {
+            author {
+              firstName
+              lastName
+            }
+          }
+        `));
         done();
       });
     });
