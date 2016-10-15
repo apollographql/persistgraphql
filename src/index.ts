4561601da3ee3296a2e859033eff653c2cc996b4
Dhaivat Pandya <dhaivatpandya@gmail.com>
2016-10-14 22:44:57 -0400

    added basic util method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
-export class ExtractGQL {
-  constructor() {
+import fs = require('fs');
 
+export interface OutputMap {
+
+}
+
+export class ExtractGQL {
+  private inputFilePath: string;
+  private outputFilePath: string;
+
+  constructor({
+    inputFilePath,
+    outputFilePath = "extracted_queries.json",
+  }: {
+    inputFilePath: string,
+    outputFilePath?: string,
+  }) {
+    this.inputFilePath = inputFilePath;
+    this.outputFilePath = outputFilePath;
+  }
+
+  // Processes an input path, which may be a path to a GraphQL file,
+  // a TypeScript file or a Javascript file. Returns a map going from
+  // a hash to a query document.
+  private processInputPath() {
+    
+  }
+
+  // Extracts GraphQL queries from this.inputFilePath and produces
+  // an output JSON file in this.outputFilePath.
+  public extract() {
+    this.processInputPath()
+  }
+
+  // Given a file path, this returns the extension of the file within the
+  // file path.
+  public static getFileExtension() {
+  }
+
+  // Checks if a given path points to a directory.
+  public static isDirectory(path: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const stats = fs.stat(path, (err, stats) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(stats.isDirectory());
+        }
+      });
+    });
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -6,7 +6,26 @@ import { ExtractGQL } from '../src/index';
 describe('ExtractGQL', () => {
   it('should be able to construct an instance', () => {
     assert.doesNotThrow(() => {
-      new ExtractGQL();
+      const egql = new ExtractGQL({
+        inputFilePath: "queries.graphql",
+        outputFilePath: "output.json",
+      });
+    });
+  });
+
+  describe('isDirectory', () => {
+    it('should return true on a directory', (done) => {
+      ExtractGQL.isDirectory('./test/fixtures').then((result) => {
+        assert(result);
+        done();
+      });
+    });
+
+    it('should return false on a file', (done) => {
+      ExtractGQL.isDirectory('./test/fixtures/queries.graphql').then((result) => {
+        assert(!result);
+        done();
+      });
     });
   });
 });
