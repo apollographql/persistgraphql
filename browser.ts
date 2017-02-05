7f15dc6d4d8a51b1292fb121ad4c4359cdef42e8
Dhaivat Pandya <dhaivat@meteor.com>
2017-02-05 16:13:51 -0500

    changed the index route

--- browser.ts
+++
@@ -1,8 +0,0 @@
-// This file is to be imported for client code using the network interface.
-import {
-  PersistedQueryNetworkInterface,
-} from './src/network_interface/ApolloNetworkInterface';
-
-export {
-  PersistedQueryNetworkInterface,
-};
