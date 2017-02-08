ca4a52ede3202d8140238e5a4a1d54a4e7176f9f
Rami Khalaf <ramikhalaf@users.noreply.github.com>
2017-02-08 09:09:47 -0800

    Use addPersistedQueries in PersistedQueryNetworkInterface

--- src/network_interface/ApolloNetworkInterface.ts
+++ src/network_interface/ApolloNetworkInterface.ts
@@ -48,45 +48,15 @@ export class PersistedQueryNetworkInterface extends HTTPFetchNetworkInterface {
     this._opts = opts;
     this.queryMap = queryMap;
     this.enablePersistedQueries = enablePersistedQueries;
-  }
 
-  // Overriden function from HTTPFetchNetworkInterface. Instead of sending down the entire
-  // query document to the server, this will only send down the id of the query
-  // from the query OutputMap.
-  public fetchFromRemoteEndpoint({
-    request,
-    options,
-  }: RequestAndOptions): Promise<IResponse> {
-    // If we are not in an enablePersistedQueries environment, this should just use the
-    // standard network interface.
-    if (!this.enablePersistedQueries) {
-      return super.fetchFromRemoteEndpoint({ request, options });
+    if (enablePersistedQueries) {
+      addPersistedQueries(this, queryMap);
     }
-
-    const queryDocument = request.query;
-    const queryKey = getQueryDocumentKey(queryDocument);
-    if (!this.queryMap[queryKey]) {
-      throw new Error('Could not find query inside query map.');
-    }
-    const serverRequest = {
-      id: this.queryMap[queryKey],
-      variables: request.variables,
-      operationName: request.operationName,
-    };
-    return fetch(this._uri, _.assign({}, this._opts, {
-      body: JSON.stringify(serverRequest),
-      method: 'POST',
-    }, options, {
-      headers: _.assign({}, {
-        Accept: '*/*',
-        'Content-Type': 'application/json',
-      }, options.headers),
-    }));
   }
 }
 
 export function addPersistedQueries(networkInterface: NetworkInterface, queryMap: OutputMap) {
-  const _query = networkInterface.query;
+  const _query = networkInterface.query.bind(networkInterface);
   return Object.assign(networkInterface, {
     query: (request: Request): Promise<ExecutionResult> => {
       const queryDocument = request.query;
