42d02f54d98463ca7a38436e5bcaef548ce92794
Evans Hauser <evanshauser@gmail.com>
2018-07-27 14:29:11 -0700

    remove all files and add links to other repositories

--- test/tests.ts
+++
@@ -1,9 +0,0 @@
-import 'es6-promise';
-process.env.NODE_ENV = 'test';
-
-import './index';
-import './extractFromAST';
-import './network_interface/ApolloNetworkInterface';
-import './queryTransformers';
-import './extractFromJS';
-import './common';
